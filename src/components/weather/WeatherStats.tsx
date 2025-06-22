c271dfd985fcf8510cc818a9b7cd6fd63fcdf578
Jakub Jirák <jirak.jakub12@seznam.cz>
2025-06-22 10:19:03 +0200

    aby to fungovalo

--- src/components/weather/WeatherStats.tsx
+++ src/components/weather/WeatherStats.tsx
@@ -1,26 +1,26 @@
-import { useWeatherContext } from "@/data/Context/WeatherContext.tsx";
-import { getText } from "@/data/Functions/weather-functions.ts";
-import { codes } from "@/data/weather-codes.ts";
-import { useMemo } from "react";
+// import { useWeatherContext } from "@/data/Context/WeatherContext.tsx";
+// import { getText } from "@/data/Functions/weather-functions.ts";
+// import { codes } from "@/data/weather-codes.ts";
+// import { useMemo } from "react";
 
 const WeatherStats = () => {
-  const { weatherData } = useWeatherContext();
+  //const { weatherData } = useWeatherContext();
 
-  const codeData = useMemo(
-    () =>
-      // @ts-ignore
-      codes.find((code) => code.code === weatherData?.current?.condition?.code),
-    [weatherData],
-  );
+  // const codeData = useMemo(
+  //   () =>
+  //     // @ts-ignore
+  //     codes.find((code) => code.code === weatherData?.current?.condition?.code),
+  //   [weatherData],
+  // );
   //@ts-ignore
-  const icontext = getText(weatherData.current.is_day, codeData) || "text";
+  //const icontext = getText(weatherData.current.is_day, codeData) || "text";
   //@ts-ignore
-  const temp = weatherData.current.temp_c || "stupen";
+  //const temp = weatherData.current.temp_c || "stupen";
 
   return (
     <div>
-      <p>{icontext}</p>
-      <p>{temp}</p>
+      <p>nesnasim tohle</p>
+      <p>nesnasim tohle</p>
     </div>
   );
 };
