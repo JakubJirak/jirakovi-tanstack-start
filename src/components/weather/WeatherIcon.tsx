2f96448232b00bbd71cecb8bc6fd35f63681ed34
Jakub Jirák <jirak.jakub12@seznam.cz>
2025-06-22 10:43:12 +0200

    zpatky

--- src/components/weather/WeatherIcon.tsx
+++ src/components/weather/WeatherIcon.tsx
@@ -1,5 +1,5 @@
 import { useWeatherContext } from "@/data/Context/WeatherContext.tsx";
-import { getImage, getText } from "@/data/Functions/weather-functions.ts";
+import { getImage } from "@/data/Functions/weather-functions.ts";
 import { codes } from "@/data/weather-codes.ts";
 import { useMemo } from "react";
 
@@ -16,10 +16,6 @@ const WeatherIcon = () => {
   if (!weatherData || !codeData) return null;
   // @ts-ignore
   const imgFile = getImage(codeData.code, weatherData.current.is_day, codeData);
-  // @ts-ignore
-  const temp = weatherData.current.temp_c;
-  // @ts-ignore
-  const icontext = getText(weatherData.current.is_day, codeData) || "text";
 
   return (
     <div className="w-full">
@@ -29,8 +25,6 @@ const WeatherIcon = () => {
         data={`/weather-icons/${imgFile}.svg`}
         type="image/svg+xml"
       />
-      <p>{temp}</p>
-      <p>{icontext}</p>
     </div>
   );
 };
