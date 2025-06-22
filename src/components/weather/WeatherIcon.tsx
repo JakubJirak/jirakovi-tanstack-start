d6b631e4ae996aa2e6d72f7e4a2180bc3b5521e9
Jakub Jirák <jirak.jakub12@seznam.cz>
2025-06-22 14:32:43 +0200

    zpatky pridat weatherfunkce

--- src/components/weather/WeatherIcon.tsx
+++ src/components/weather/WeatherIcon.tsx
@@ -1,5 +1,34 @@
+import { useWeatherContext } from "@/data/Context/WeatherContext.tsx";
+import { getImage } from "@/data/Functions/weather-functions.ts";
+import { codes } from "@/data/weather-codes.ts";
+import { useMemo } from "react";
+
 const WeatherIcon = () => {
-  return <div>dasdas</div>;
+  const { weatherData } = useWeatherContext();
+
+  const codeData = useMemo(
+    () =>
+      codes.find((code) => code.code === weatherData?.current?.condition?.code),
+    [weatherData],
+  );
+
+  if (!weatherData || !codeData) return null;
+  const imgFile = getImage(
+    codeData?.code,
+    weatherData?.current?.is_day,
+    codeData,
+  );
+
+  return (
+    <div className="w-full">
+      <object
+        height="250px"
+        aria-label="objekt"
+        data={`/weather-icons/${imgFile}.svg`}
+        type="image/svg+xml"
+      />
+    </div>
+  );
 };
 
 export default WeatherIcon;
