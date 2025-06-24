d285d3876fe283b192fa3c0bc0724809ac94ed24
Jakub Jirák <jirak.jakub12@seznam.cz>
2025-06-24 22:17:38 +0200

    timeouty pro funkce weather

--- src/data/Functions/weather-functions.ts
+++ src/data/Functions/weather-functions.ts
@@ -7,20 +7,24 @@ export interface codeData {
 }
 
 export function getImage(code?: number, day?: number, codeData?: codeData) {
-  if (!code || !day || !codeData) return "null";
+  setTimeout(() => {
+    if (!code || !day || !codeData) return "null";
 
-  if (code === 1000 || code === 1003) {
-    if (day === 1) {
-      return codeData.icon;
-      // biome-ignore lint/style/noUselessElse: <explanation>
-    } else {
-      return codeData.nighticon;
+    if (code === 1000 || code === 1003) {
+      if (day === 1) {
+        return codeData.icon;
+        // biome-ignore lint/style/noUselessElse: <explanation>
+      } else {
+        return codeData.nighticon;
+      }
     }
-  }
-  return codeData.icon;
+    return codeData.icon;
+  }, 500);
 }
 
 export function getText(day?: number, codeData?: codeData) {
-  if (!day || !codeData) return "null";
-  return day === 1 ? codeData.day : codeData.night;
+  setTimeout(() => {
+    if (!day || !codeData) return "null";
+    return day === 1 ? codeData.day : codeData.night;
+  }, 500);
 }
