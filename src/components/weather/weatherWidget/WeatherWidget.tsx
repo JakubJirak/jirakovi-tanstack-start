5742c80ab0e3e256799668061714d37877cec537
Jakub Jirák <jirak.jakub12@seznam.cz>
2025-06-24 22:13:58 +0200

    icon loading

--- src/components/weather/weatherWidget/WeatherWidget.tsx
+++ src/components/weather/weatherWidget/WeatherWidget.tsx
@@ -1,9 +1,13 @@
-import {useWeatherContext} from "@/data/Context/WeatherContext.tsx";
-import {type codeData, getImage, getText} from "@/data/Functions/weather-functions.ts";
 import WeatherWidgetRow from "@/components/weather/weatherWidget/WeatherWidgetRow.tsx";
+import { useWeatherContext } from "@/data/Context/WeatherContext.tsx";
+import {
+  type codeData,
+  getImage,
+  getText,
+} from "@/data/Functions/weather-functions.ts";
 
 interface WeatherWidget {
-    codeData: codeData | undefined;
+  codeData: codeData | undefined;
 }
 
 /*
@@ -17,42 +21,46 @@ DEN - min. temp, max. temp, daily chance of rain, date
 HODINA - icon (isday), texticon, temp_c, wind_kph, chance of rain
 */
 
-const WeatherWidget = ({codeData}:WeatherWidget) => {
-    const { weatherData } = useWeatherContext();
+const WeatherWidget = ({ codeData }: WeatherWidget) => {
+  const { weatherData } = useWeatherContext();
 
-    if (!weatherData || !codeData) return <p>Chyba pri nacitani dat</p>;
+  if (!weatherData || !codeData) return <p>Chyba pri nacitani dat</p>;
 
-    const imgFile = getImage(
-        codeData?.code,
-        weatherData?.current?.is_day,
-        codeData,
-    );
+  const imgFile = getImage(
+    codeData?.code,
+    weatherData?.current?.is_day,
+    codeData,
+  );
 
-    const icontext = getText(weatherData?.current?.is_day, codeData);
-    const city = weatherData?.location?.name;
-    const updated = weatherData?.location?.localtime;
-    const temp = weatherData.current?.temp_c;
-    const feelsLike = String(weatherData.current?.feelslike_c) + ' °C';
-    const humidity = String(weatherData.current?.humidity) + '%';
-    const wind = String(weatherData.current?.wind_kph) + ' km/h';
+  const icontext = getText(weatherData?.current?.is_day, codeData);
+  const city = weatherData?.location?.name;
+  const updated = weatherData?.location?.localtime;
+  const temp = weatherData.current?.temp_c;
+  const feelsLike = `${String(weatherData.current?.feelslike_c)} °C`;
+  const humidity = `${String(weatherData.current?.humidity)} %`;
+  const wind = `${String(weatherData.current?.wind_kph)} km/h`;
 
-    return (
-        <div className='flex gap-2'>
-            <div className="">
-                <p>{city}</p>
-                <div className="w-50">
-                    <img width="100%" src={`../../../public/weather-icons/${imgFile}.svg`} alt={imgFile} />
-                </div>
-                <p>{icontext}</p>
-            </div>
-            <div className="">
-                <p>{updated}</p>
-                <p>{temp} °C</p>
-                <WeatherWidgetRow icon='i' title='Pocitova' value={feelsLike}/>
-                <WeatherWidgetRow icon='i' title='Vlhkost' value={humidity}/>
-                <WeatherWidgetRow icon='i' title='Vitr' value={wind}/>
-            </div>
+  return (
+    <div className="flex gap-2">
+      <div className="">
+        <p>{city}</p>
+        <div className="w-50">
+          <img
+            width="100%"
+            src={`/weather-icons/${imgFile}.svg`}
+            alt={imgFile}
+          />
         </div>
-    )
-}
+        <p>{icontext}</p>
+      </div>
+      <div className="">
+        <p>{updated}</p>
+        <p>{temp} °C</p>
+        <WeatherWidgetRow icon="i" title="Pocitova" value={feelsLike} />
+        <WeatherWidgetRow icon="i" title="Vlhkost" value={humidity} />
+        <WeatherWidgetRow icon="i" title="Vitr" value={wind} />
+      </div>
+    </div>
+  );
+};
 export default WeatherWidget;
