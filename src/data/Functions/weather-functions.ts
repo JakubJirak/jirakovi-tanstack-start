e48008c0f833dd6bcbd4b8908f18cfe0d54b7168
Jakub Jirák <jirak.jakub12@seznam.cz>
2025-06-26 22:11:56 +0200

    debilni chyba

--- src/data/Functions/weather-functions.ts
+++ src/data/Functions/weather-functions.ts
@@ -7,25 +7,20 @@ export interface codeData {
 }
 
 export function getImage(code?: number, day?: number, codeData?: codeData) {
-  try {
-    if (code === 1000 || code === 1003) {
-      if (day === 1) {
-        return codeData?.icon;
-        // biome-ignore lint/style/noUselessElse: <explanation>
-      } else {
-        return codeData?.nighticon;
-      }
+  if (!code || !day || !codeData) return "null";
+
+  if (code === 1000 || code === 1003) {
+    if (day === 1) {
+      return codeData.icon;
+      // biome-ignore lint/style/noUselessElse: <explanation>
+    } else {
+      return codeData.nighticon;
     }
-    return codeData?.icon;
-  } catch (e) {
-    return "null";
   }
+  return codeData.icon;
 }
 
 export function getText(day?: number, codeData?: codeData) {
-  try {
-    return day === 1 ? codeData?.day : codeData?.night;
-  } catch (e) {
-    return "null";
-  }
+  if (!day || !codeData) return "null";
+  return day === 1 ? codeData.day : codeData.night;
 }
